636e4c92150391f68be9a094157efb3fb3a09837
JoelSDS <joel.santos-dasilva@hotmail.com>
2025-06-17 20:45:30 +0200

    upload and updateImages

--- backend/routes/projects.route.js
+++ backend/routes/projects.route.js
@@ -1,11 +1,15 @@
-const projectController = require('../controllers/projects.controller');
-const express = require('express');
+const projectController = require("../controllers/projects.controller");
+const express = require("express");
 const router = express.Router();
 
-router.post('/findAll', projectController.findAll);
-router.post('/', projectController.create);
-router.put('/:id', projectController.update);
-router.put('/:id', projectController.delete);
-router.delete('/:id', projectController.deleteHard);
+router.post("/findAll", projectController.findAll);
+router.post("/", projectController.create);
+router.put("/:id", projectController.update);
+router.put("/:id", projectController.delete);
+router.delete("/:id", projectController.deleteHard);
+
+// A FAIRE : AJOUT DES PHOTOS ET MODIFICATION DES PHOTOS
+router.post("/upload/:id", projectController.uploadImage);
+router.put("/updateImages/:id", projectController.updateImages);
 
 module.exports = router;
