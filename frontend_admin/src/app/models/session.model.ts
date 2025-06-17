d31a74d1d13fee93cff0fbb14f3336804e058825
Ryen-K <ryen.kamkoum@gmail.com>
2025-06-17 20:03:28 +0200

    create service & model for sessions

---
+++ frontend_admin/src/app/models/session.model.ts
@@ -0,0 +1,7 @@
+export interface Session {
+    _id: string;
+    email: string;
+    status: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ frontend_admin/src/app/services/session.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Session } from '../models/session.model';
+import { environment } from '../environement';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+
+  private baseUrl = environment.BASE_URL + '/sessions';
+
+  constructor(private http: HttpClient) {}
+
+  getSessions(): Observable<Session[]> {
+    return this.http.post<Session[]>(
+      this.baseUrl,
+      {},
+      {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      }
+    );  
+  }
+}
